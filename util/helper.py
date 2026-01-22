23709af4a67286259a3e9ad23756b746274edafb
Robb Northrup <robbnorthrup@outlook.com>
2026-01-22 12:48:16 -0800

    methodology for classifying gaiadr3 stars

--- util/helper.py
+++ util/helper.py
@@ -31,3 +31,29 @@ def basic_exploration(my_df, name):
     print('~~~~~~~~~~~~~~~~~')
     for col in my_df:
         print(my_df[col].value_counts())
+
+
+# Classify stars by temperature for available TEFF data
+# Needs to be adjusted to account for magnitude as well
+def spectype_from_teff(row):
+    # spectype already . . .
+    if not pd.isnull(row["st_spectype"]):
+        return row["st_spectype"][0]
+    # else, assign based on temperatures. . .
+    elif pd.isnull(row["st_spectype"]) and row['st_teff'] > 30000:
+        return "O"
+    elif pd.isnull(row["st_spectype"]) and 10000 < row['st_teff'] <= 30000:
+        return "B"
+    elif pd.isnull(row["st_spectype"]) and 7500 < row['st_teff'] <= 10000:
+        return "A"
+    elif pd.isnull(row["st_spectype"]) and 6000 < row['st_teff'] <= 7500:
+        return "F"
+    elif pd.isnull(row["st_spectype"]) and 5200 < row['st_teff'] <= 6000:
+        return "G"
+    elif pd.isnull(row["st_spectype"]) and 3700 < row['st_teff'] <= 5200:
+        return "K"
+    elif pd.isnull(row["st_spectype"]) and row['st_teff'] <= 3700:
+        return "M"
+    # if no temp value and no spectype . . . 
+    else:
+        return "UNKNOWN"
